1535d08ff7b6c6d99e3c371b8d1f08e4cd541f72
brettkolodny <brettkolodny@gmail.com>
2020-04-12 13:30:02 -0400

    Started repl

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,29 @@ mod token;
 #[cfg(test)]
 mod tests;
 
+use crate::lexer::Lexer;
+use crate::token::TokenType;
+use std::io;
+
 fn main() {
-    println!("Welcome to Slice");
+    loop {
+        print!(">> ");
+        io::Write::flush(&mut io::stdout()).expect("flush failed!");
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let mut lexer = Lexer::new(input.as_str());
+                let mut token = lexer.next();
+
+                while token.token_type != TokenType::EOF {
+                    print!("{} ", token);
+                    token = lexer.next();
+                }
+
+                println!();
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
